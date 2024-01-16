4ec8f66407a97dabadd178459755518f048d45cd
AleksandrAleksejev <93324220+AleksandrAleksejev@users.noreply.github.com>
2024-01-16 11:57:55 +0200

    Update conf.php
    conf

--- tantsudtahtedega/conf.php
+++ tantsudtahtedega/conf.php
@@ -1,6 +1,15 @@
 <?php
-$baasiaadress="localhost";
-$baasikasutaja="Tarpv22";
-$baasiparool="123456";
-$baasinimi="tantsudtahtuda";
-$yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);
+
+$yhendus = new mysqli("d113361.mysql.zonevs.eu", "d113361_kasutaja", "Kukuruku813577", "d113361_aleksejev");
+$yhendus->set_charset('UTF8');
+
+/*
+ * create table tantsud(
+    id int primary key auto_increment,
+    tantsupaar varchar(25) not null,
+    punktid int,
+    komentaarid text,
+    avalik int default 1,
+    avaliku_paev datetime
+);
+ */
